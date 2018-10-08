c6b16ba7bec08012c259f1e03026286d5f5ff640
William Ervin <william.ervin.14@cnu.edu>
2018-10-07 21:54:45 -0400

    updating files

--- register.php
+++ register.php
@@ -10,11 +10,10 @@
   <form method="post" action="register.php">
   	
   	<div class="container">
-	
+	  <?php include('errors.php'); ?>
 	  <h1>Sign Up</h1>
 	  <p>Please fill in this form to create an account.</p>
 	  <hr>
-	  <?php include('errors.php'); ?>
   	  <label>Username</label>
   	  <input type="text" placeholder="Enter Username" name="username" value="<?php echo $username; ?>">
   	
