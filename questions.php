153463afb95385cdb76a7cd73524cb3b05ea8991
William Ervin <william.ervin.14@cnu.edu>
2018-10-08 14:19:46 -0400

    adding questions.php

--- questions.php
+++ questions.php
@@ -19,6 +19,13 @@
 		<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
 		<input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other 
 	  <br><br>
+	  Question one:
+		<input type="radio" name="q1" <?php if (isset($q1) && $q1=="one") echo "checked";?> value="one">One
+		<input type="radio" name="q1" <?php if (isset($q1) && $q1=="two") echo "checked";?> value="two">Two
+		<input type="radio" name="q1" <?php if (isset($q1) && $q1=="three") echo "checked";?> value="three">Three 
+		<input type="radio" name="q1" <?php if (isset($q1) && $q1=="four") echo "checked";?> value="four">Four
+		<input type="radio" name="q1" <?php if (isset($q1) && $q1=="five") echo "checked";?> value="five">Five 
+	  <br><br>
  
   	  <div class="clearfix">
 			<button type="submit" name="questions_user" class="signupbtn">Submit</button>
