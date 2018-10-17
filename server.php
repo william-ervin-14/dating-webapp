0e456baae92fa7bbfd83964434d6aab7a9e88d2a
Ervin <william.ervin.14@cnu.edu>
2018-10-17 16:28:23 -0400

    editing index.php

--- server.php
+++ server.php
@@ -76,8 +76,10 @@ if (isset($_POST['login_user'])) {
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['email'] = $email;
-	  $_SESSION['firstname'] = $firstname;
-	  $_SESSION['lastname'] = $lastname;
+	  $queryUser = "SELECT * FROM users WHERE email='$email'";
+	  $row = mysql_fetch_array($query);
+	  $_SESSION['firstname'] = $row['firstname'];
+	  $_SESSION['lastname'] = $row['lastname'];
   	  $_SESSION['success'] = "You are now logged in";
   	  header('location: index.php');
   	}else {
