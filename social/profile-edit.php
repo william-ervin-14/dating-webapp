674491efe68614429a7468fe5b4fbc206f38c8c2
William Ervin <william.ervin.14@cnu.edu>
2018-10-23 19:39:53 -0400

    testing db tables

--- social/profile-edit.php
+++ social/profile-edit.php
@@ -2,7 +2,7 @@
 	require_once('includes/class-query.php');
 	require_once('includes/class-insert.php');
 	
-	$logged_user_id = 1;
+	$logged_user_id = 45;
 	
 	if ( !empty ( $_POST ) ) {
 		$update = $insert->update_user($logged_user_id, $_POST);
