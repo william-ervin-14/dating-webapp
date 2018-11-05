18ca3a83304ea7e82b63006a9aa29ae21a4ef31f
William Ervin <william.ervin.14@cnu.edu>
2018-11-05 16:47:30 -0500

    working on uid

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php 
-  if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
+<?php  
+  session_start(); 
+  
   require_once('server.php');
   require_once('includes/class-insert.php');
   require_once('includes/class-query.php');
