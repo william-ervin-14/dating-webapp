2f13ee5d856f8ef483250ed55ce4d06e10006132
Ervin <william.ervin.14@cnu.edu>
2018-11-07 13:50:30 -0500

    working with uid and sessions

--- includes/class-login.php
+++ includes/class-login.php
@@ -27,8 +27,7 @@ if ( !class_exists('Login') ){
 			return false;
 		}
 		public function verify_session() {
-			//$email = $_SESSION['email'];
-			$email = 'tester@tester.com';
+			$email = $_SESSION['email'];
 			$user = $this->user_exists($email);
 		
 			if( $user != false){
