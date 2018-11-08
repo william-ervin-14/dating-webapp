4a1272a9bd10ae28f5733532f48b38eb05b6960b
Ervin <william.ervin.14@cnu.edu>
2018-11-08 14:02:25 -0500

    working with uid and sessions

--- includes/class-db.php
+++ includes/class-db.php
@@ -1,6 +1,7 @@
 <?php
 	if ( !class_exists ('DB') ) {
 		class DB {
+		    public $results;
 			public function __construct() {
 				$mysqli = new mysqli('cpsc498.c4gfuryc8w4w.us-east-1.rds.amazonaws.com', 'WillAdmin', 'C@pstone498', 'accounts');
 				
@@ -30,15 +31,14 @@
 				if ( !$result ) {
 					return false;
 				}
-				
+
 				while ( $obj = $result->fetch_object() ) {
 					$results[] = $obj;
 				}
 				
-				return $results;
+				return $this->results;
 			}
 		}
 	}
 	
 	$db = new DB;
-?>
