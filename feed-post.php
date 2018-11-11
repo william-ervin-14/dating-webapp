c137f3dfaf365526ade3cd1604164d870807dadf
William Ervin <william.ervin.14@cnu.edu>
2018-11-11 17:11:20 -0500

    created header.php

--- feed-post.php
+++ feed-post.php
@@ -9,27 +9,6 @@
 		$add_status = $insert->add_status($logged_user_id, $_POST);
 	}
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Post Status</title>
-		<link rel="stylesheet" href="css/style.css" />
-	</head>
-	<body>
-		<div id="navigation">
-			<ul>
-				<li><a href="home.php">Home</a></li>
-				<li><a href="profile-view.php">View Profile</a></li>
-				<li><a href="profile-edit.php">Edit Profile</a></li>
-				<li><a href="friends-directory.php">Member Directory</a></li>
-				<li><a href="friends-list.php">Friends List</a></li>
-				<li><a href="feed-view.php">View Feed</a></li>
-				<li><a href="feed-post.php">Post Status</a></li>
-				<li><a href="messages-inbox.php">Inbox</a></li>
-				<li><a href="messages-compose.php">Compose</a></li>
-				<li><a href="login.php" name="logout">Log out</a></li>
-			</ul>
-		</div>
 		<h1>Post Status</h1>
 		<div class="content">
 			<form method="post">
--- messages-inbox.php
+++ messages-inbox.php
@@ -6,27 +6,6 @@
 	$logged_user_id = 45;
 	
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Inbox</title>
-		<link rel="stylesheet" href="css/style.css" />
-	</head>
-	<body>
-		<div id="navigation">
-			<ul>
-				<li><a href="home.php">Home</a></li>
-				<li><a href="profile-view.php">View Profile</a></li>
-				<li><a href="profile-edit.php">Edit Profile</a></li>
-				<li><a href="friends-directory.php">Member Directory</a></li>
-				<li><a href="friends-list.php">Friends List</a></li>
-				<li><a href="feed-view.php">View Feed</a></li>
-				<li><a href="feed-post.php">Post Status</a></li>
-				<li><a href="messages-inbox.php">Inbox</a></li>
-				<li><a href="messages-compose.php">Compose</a></li>
-				<li><a href="login.php" name="logout">Log out</a></li>
-			</ul>
-		</div>
 		<h1>Inbox</h1>
 		<div class="content">
 			<?php $query->do_inbox($logged_user_id); ?>
