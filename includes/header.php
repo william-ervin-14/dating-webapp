d8fdd7f0b6fc5afe31e328ffc8a4f56344dbaca3
William Ervin <william.ervin.14@cnu.edu>
2018-11-11 19:22:53 -0500

    working with uid

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,9 @@
 <?php
+require_once('class-login.php');
+require_once('class-query.php');
+require_once('class-insert.php');
+require_once('class-db.php');
+
 $email = $_SESSION['email'];
 $user_temp = $query->load_user_objects_by_email ($email);
 $logged_user_id = ($user_temp->ID);
@@ -13,7 +18,7 @@ $logged_user_id = ($user_temp->ID);
 		<div id="navigation">
 			<ul>
 				<li><a href="home.php">Home</a></li>
-				<li><a href="profile-view.php">View Profile</a></li>
+				<li><a href="profile-view.php?uid=<?php echo $logged_user_id; ?>">View Profile</a></li>
 				<li><a href="profile-edit.php">Edit Profile</a></li>
 				<li><a href="friends-directory.php">Member Directory</a></li>
 				<li><a href="friends-list.php">Friends List</a></li>
