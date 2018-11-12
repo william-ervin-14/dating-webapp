277197ae2ef20df245bb77cc868fbd939dcfb8ae
William Ervin <william.ervin.14@cnu.edu>
2018-11-11 21:00:19 -0500

    working with uid

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,12 @@
 <?php
-$email = $_SESSION['email'];
-$user = $query->load_user_objects_by_email ($email);
-$logged_user_id = ($user->ID);
+    require_once('class-query.php');
+    require_once('class-insert.php');
+    require_once('class-db.php');
+    require_once('class-login.php');
+    
+    $email = $_SESSION['email'];
+    $user = $query->load_user_objects_by_email ($email);
+    $logged_user_id = ($user->ID);
 ?>
 
 <!DOCTYPE html>
