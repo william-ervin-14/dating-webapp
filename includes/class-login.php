52f507b29f9fc407bbef54a4338e9666cff3d636
William Ervin <william.ervin.14@cnu.edu>
2018-11-12 14:58:10 -0500

    working with friends list

--- includes/class-login.php
+++ includes/class-login.php
@@ -24,21 +24,23 @@ if ( !class_exists('Login') ){
                     return false;
                 }
 
-                $email = mysqli_real_escape_string($this->db->connection, $post['email']);
-                $password = md5(mysqli_real_escape_string($this->db->connection, $post['password']));
+                $email    = mysqli_real_escape_string($this->db->connection, $post['email']);
+                $password = mysqli_real_escape_string($this->db->connection, $post['password']);
 
                 $results = $this->query->load_single_user_by_email_password($email, $password);
+
                 if (mysqli_num_rows($results) == 1) {
                     $_SESSION['email'] = $email;
                     return true;
                 }
+
                 return false;
             }
         }
 		public function verify_session() {
             if (isset($_SESSION['email'])) {
                 $email = $_SESSION['email'];
-                $user = $this->user_exists($email);
+                $user  = $this->user_exists($email);
 
                 if (false !== $user) {
                     $this->user = $user;
@@ -49,25 +51,28 @@ if ( !class_exists('Login') ){
 		}
 		public function register($post) {
             if (isset($_POST['reg_user'])) {
+
+                $email      = $post['email'];
+                $first_name = $post['firstname'];
+                $last_name  = $post['lastname'];
+                $password   = $post['password_1'];
+
                 if ($post['password_2'] !== $post['password_1']) {
                     return "two passwords do not match";
                 }
-                $email = $post['email'];
-                //$query = "SELECT * FROM users WHERE email='$email'";
-                //$results = mysqli_query($this->db->connection, $query);
+
                 $results = $this->query->load_single_user_by_email($email);
+
                 if (mysqli_num_rows($results) == 1) {
                     return "Email already exists";
                 }
-                $firstname = $post['firstname'];
-                $lastname  = $post['lastname'];
-                $password  = md5($post['password_1']);
 
-                $insert = $this->insert->register_user($firstname, $lastname, $email, $password);
+                $insert = $this->insert->register_user($first_name, $last_name, $email, $password);
 
                 if (false !== $insert) {
                     return "Account created successfully.";
                 }
+
                 return "An unknown error has occurred";
             }
         }
