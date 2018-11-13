b3fe6e15fafa38c915d557beaebf50a9463dadec
William Ervin <william.ervin.14@cnu.edu>
2018-11-13 18:03:10 -0500

    working with messages

--- messages.php
+++ messages.php
@@ -34,7 +34,7 @@
     }
 ?>
         <h1>Messages</h1>
-        <h6><?php echo $different_friends[0]->friend_id; ?></h6>
+        <h6><?php echo $different_friends[0]->firstname; ?></h6>
         <div class="content">
             <form method="post">
                 <div class="verticalTabs">
