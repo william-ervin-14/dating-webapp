1842368ca144db0a1348a5bf7f97eba3a65b0c0d
William Ervin <william.ervin.14@cnu.edu>
2018-11-12 21:19:14 -0500

    working with messages

--- messages.php
+++ messages.php
@@ -23,8 +23,10 @@
     foreach ( $friend_ids as $friend_id ) {
         $friend_objects[] = $query->load_user_object($friend_id);
     }
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $send_message = $insert->send_message($_POST['message_time'], $_POST['message_sender_id'], $_POST['message_recipient_id'], $_POST['message_content']);
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['message_content']) && isset($_POST['message_content'])) {
+            $send_message = $insert->send_message($_POST['message_time'], $_POST['message_sender_id'], $_POST['message_recipient_id'], $_POST['message_content']);
+        }
     }
 ?>
         <h1>Messages</h1>
