b01369e16b3e8d2b5f85ed523e8f40b08bdb4b4c
William Ervin <william.ervin.14@cnu.edu>
2018-11-14 13:34:00 -0500

    messages css

--- messages.php
+++ messages.php
@@ -24,6 +24,14 @@
         $friend_objects[] = $query->load_user_object($friend_id);
     }
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['new_message_content']) && isset($_POST['new_message_recipient_id'])) {
+            $send_message = $insert->send_message($_POST['new_message_time'], $_POST['new_message_sender_id'], $_POST['new_message_recipient_id'], $_POST['new_message_content']);
+            unset($_POST['new_message_time']);
+            unset($_POST['new_message_sender_id']);
+            unset($_POST['new_message_recipient_id']);
+            unset($_POST['new_message_content']);
+            header('location: messages.php');
+        }
         if (!empty($_POST['message_content']) && isset($_POST['message_recipient_id'])) {
             $send_message = $insert->send_message($_POST['message_time'], $_POST['message_sender_id'], $_POST['message_recipient_id'], $_POST['message_content']);
             unset($_POST['message_time']);
@@ -46,11 +54,11 @@
                 </div>
 
                 <div id="New Message" class="tab_content">
-                    <input name="message_time" type="hidden" value="<?php echo time(); ?>" />
-                    <input name="message_sender_id" type="hidden" value="<?php echo $logged_user_id; ?>" />
+                    <input name="new_message_time" type="hidden" value="<?php echo time(); ?>" />
+                    <input name="new_message_sender_id" type="hidden" value="<?php echo $logged_user_id; ?>" />
                     <p>
-                        <label for="message_recipient_id">To:</label>
-                        <select name="message_recipient_id">
+                        <label for="new_message_recipient_id">To:</label>
+                        <select name="new_message_recipient_id">
                             <option value="">--Select a Friend--</option>
                             <?php foreach ( $friend_objects as $friend ) : ?>
                                 <option value="<?php echo $friend->ID; ?>"><?php echo "{$friend->firstname} {$friend->lastname}"; ?></option>
@@ -58,8 +66,8 @@
                         </select>
                     </p>
                     <p>
-                        <label for="message_content">Message:</label>
-                        <textarea name="message_content"></textarea>
+                        <label for="new_message_content">Message:</label>
+                        <textarea name="new_message_content"></textarea>
                     </p>
                     <p>
                         <input type="submit" value="Submit" />
