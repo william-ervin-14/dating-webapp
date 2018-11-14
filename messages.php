1a997293288937358d9bddd9a1de77e967b71fa7
Ervin <william.ervin.14@cnu.edu>
2018-11-14 17:56:13 -0500

    working on messaging system

--- messages.php
+++ messages.php
@@ -66,16 +66,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <?php if(in_array($message_temp,$message_received_objects)) :?>
                         <div class="message-box-received">
                             <p><?php echo $message_temp->message_content; ?></p>
+                            <p><?php echo $message_temp->message_time; ?></p>
                         </div>
                         <div class="message-time-received">
-                            <p><?php echo $message_temp->message_time; ?></p>
+                            
                         </div>
                     <?php else : ?>
                         <div class="message-box-sent">
                             <p><?php echo $message_temp->message_content; ?></p>
+                            <p><?php echo $message_temp->message_time; ?></p>
                         </div>
                         <div class="message-time-sent">
-                            <p><?php echo $message_temp->message_time; ?></p>
+
                         </div>
                     <?php endif ?>
                 <?php endforeach; ?>
