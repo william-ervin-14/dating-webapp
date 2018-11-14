1593a628b674144183cd14dac1a4fe16e22cfbd9
Ervin <william.ervin.14@cnu.edu>
2018-11-14 16:42:51 -0500

    working on messaging system

--- messages.php
+++ messages.php
@@ -51,7 +51,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="message_content">
     <form method="post">
 
-        <div id="navigation">
+        <div id="vertical-navigation">
             <?php foreach ($different_friends as $friend ) : ?>
                 <ul>
                     <li><a href="messages.php?uid=<?php echo $friend->ID ?>"><?php echo "{$friend->firstname} {$friend->lastname}"; ?></a></li>
