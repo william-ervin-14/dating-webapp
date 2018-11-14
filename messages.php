fa54fea85e97ab00d4927dd4846b99fbf95c632a
William Ervin <william.ervin.14@cnu.edu>
2018-11-14 12:17:35 -0500

    messages css

--- messages.php
+++ messages.php
@@ -90,7 +90,7 @@
                                 <input name="message_time" type="hidden" value="<?php echo time(); ?>" />
                                 <input name="message_sender_id" type="hidden" value="<?php echo $logged_user_id; ?>" />
                                 <input type="text" placeholder="Your message">
-                                <button type="submit">Send</button>
+                                <button class="submit_button" type="submit">Send</button>
                             </div>
                         </div>
                     <?php endforeach; ?>
