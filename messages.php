8091e4197afa3e1c0ed570f0a682f0fde49fcd07
Ervin <william.ervin.14@cnu.edu>
2018-11-14 17:15:02 -0500

    working on messaging system

--- messages.php
+++ messages.php
@@ -86,23 +86,5 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         </div>
     </form>
-    <script>
-        function openVerticalTab(evt, tabName) {
-            var i, tab_content, tab_links;
-
-            tab_content = document.getElementsByClassName("tab_content");
-            for (i = 0; i < tab_content.length; i++) {
-                tab_content[i].style.display = "none";
-            }
-            tab_links = document.getElementsByClassName("tab_links");
-            for (i = 0; i < tab_links.length; i++) {
-                tab_links[i].className = tab_links[i].className.replace(" active", "");
-            }
-
-            document.getElementById(tabName).style.display = "block";
-            evt.currentTarget.className += " active";
-        }
-        document.getElementById("defaultOpen").click();
-    </script>
 </body>
 </html>
