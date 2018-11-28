c7b8315a33c1f04771114f1904c059388dfce3d0
William Ervin <william.ervin.14@cnu.edu>
2018-11-27 19:53:37 -0500

    youtube.com

--- youtube.php
+++ youtube.php
@@ -4,7 +4,7 @@
 <body>
 <iframe id="existing-iframe-example"
         width="640" height="360"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1"
+        src="https://www.youtube.com"
         frameborder="0"
         style="border: solid 4px #37474F"
 ></iframe>
