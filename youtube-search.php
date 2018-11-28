b678f75fbd949bf2e95171348c7a3908759cf914
Ervin <william.ervin.14@cnu.edu>
2018-11-28 16:15:42 -0500

    youtube.com

--- youtube-search.php
+++ youtube-search.php
@@ -52,7 +52,7 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
     * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
     * Please ensure that you have enabled the YouTube Data API for your project.
     */
-    $DEVELOPER_KEY = 'REPLACE_ME';
+    $DEVELOPER_KEY = 'AIzaSyCDQM84XUFkyA6__WNdffCvmMzYoiaA6og';
 
     $client = new Google_Client();
     $client->setDeveloperKey($DEVELOPER_KEY);
