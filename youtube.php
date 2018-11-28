7414ade1dd8f4edd416913c1afb5fd9858831438
William Ervin <william.ervin.14@cnu.edu>
2018-11-27 19:56:20 -0500

    youtube.com

--- youtube.php
+++ youtube.php
@@ -1,4 +1,21 @@
 <?php
+
+    require_once('load.php');
+    include('includes/header.php');
+
+    if (!isset($_SESSION['email'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+    if (isset($_GET['logout'])) {
+        unset($_SESSION['email']);
+        session_destroy();
+        header("location: login.php");
+    }
+    $email = $_SESSION['email'];
+    $user = $query->load_user_objects_by_email($email);
+    $logged_user_id = ($user->ID);
+
 ?>
 <html>
 <body>
