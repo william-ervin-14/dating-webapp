48b092b2beb0aa6dbce3e520bc9a63d6bc9c7766
William Ervin <william.ervin.14@cnu.edu>
2018-11-28 18:42:47 -0500

    youtube search

--- youtube-search.php
+++ youtube-search.php
@@ -94,7 +94,7 @@ END;
     <div>
     </div>
         <?php foreach ($thumbnails as $thumbnail): ?>
-            <?php echo $thumbnail; ?>
+            <?php echo "<img src='$thumbnail'><br>"; ?>
         <?php endforeach; ?>
     </div>
     </body>
