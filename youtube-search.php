171adfd1a0b5fb9830954c08d3ff8bbb931c5a24
Ervin <william.ervin.14@cnu.edu>
2018-11-28 15:22:15 -0500

    youtube.com

--- youtube-search.php
+++ youtube-search.php
@@ -1,45 +1,97 @@
 <?php
 ?>
-<!doctype html>
 <html>
-  <head>
-  <title>Search</title>
-  </head>
-  <body>
-    <div id="buttons">
-    <label> <input id="query" value='cats' type="text"/><button id="search-button"    onclick="keyWordsearch()">Search</button></label>
-    <div id="container">
-    <h1>Search Results</h1>
-    <ul id="results"></ul>
-    </div>
 <script>
- function keyWordsearch(){
-     gapi.client.setApiKey('api_key_here');
-     gapi.client.load('youtube', 'v3', function(){
-         makeRequest();
-     });
- }
-function makeRequest(){
-    var q = $('#query').val();
-    var request = gapi.client.youtube.search.list({
-        q: q,
-            part: 'snippet',
-            maxResults: 10
-    });
-    request.execute(function(response)  {
-        $('#results').empty()
-            var srchItems = response.result.items;
-            $.each(srchItems, function(index, item){
-                vidTitle = item.snippet.title;
-                vidThumburl =  item.snippet.thumbnails.default.url;
-            vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';
 
-            $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');
-    })
-  })
-}
- </script>
- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
- <script src="https://apis.google.com/js/client.js?onload=googleApiClientReady">   </script>
-</body>
+    var vquery, nextPageToken, prevPageToken;
+
+    function searchvideo(vquery, pageToken) {
+        $('ol').empty();
+        gapi.client.setApiKey('AIzaSyCDQM84XUFkyA6__WNdffCvmMzYoiaA6og');
+        gapi.client.load('youtube', 'v3', function(){
+            var vquery = $('#vquery').val(); // Your input box
+
+            var requestOptions = {
+                q: vquery,
+                part: 'snippet',
+                maxResults: 20, // Change results number here
+                type: 'video', // Notice that type used is video only
+                pageToken: pageToken
+            };
+
+            if (pageToken) {
+                requestOptions.pageToken = pageToken;
+            }
+
+            var request = gapi.client.youtube.search.list(requestOptions);
+
+            request.execute(function(response) {
+
+                nextPageToken = response.result.nextPageToken;
+                var nextVis = nextPageToken ? 'visible' : 'hidden';
+                $('#NEXTbutton').css('visibility', nextVis);
+                prevPageToken = response.result.prevPageToken
+                var prevVis = vprevPageToken ? 'visible' : 'hidden';
+                $('#PREVbutton').css('visibility', prevVis);
+
+// This part displays your results:
+
+                for(var i=0;i<response.items.length;i++) {
+                    var rThumbnail = response.items[i].snippet.thumbnails.default.url;
+                    var rVideoID = response.items[i].id.videoId; // or snippet.videoId
+                    var rTitle = response.items[i].snippet.title;
+                    var rDescription = response.items[i].snippet.description;
+
+                    $('ol').append('<li"><img src="'+rThumbnail+'" alt="'+rVideoID+'" />
+                        <a href="javascript:void(0)" alt="'+rVideoID+'">'+rTitle+'</a>
+                        <div class="desc">'+rDescription+'</div></li>');
+
+                    $('.desc').each(function(){ // I use this in case no description
+                        if ($(this).text() === '') {
+                            $(this).text('- No description available -');
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    function nextPage() {
+        searchvideo(vquery, nextPageToken);
+    }
+    function previousPage() {
+        searchvideo(vquery, prevPageToken);
+    }
+
+</script>
+
+
+<input id="vquery" type="text"/>
+<button id="PREVbutton" onclick="previousPage();" class="YTSpagers">Prev</button>
+<button id="NEXTbutton" onclick="nextPage();" class="YTSpagers">Next</button>
+
+<ol></ol>
+
+
+<script>
+
+    $('.YTSpagers').css('visibility', 'hidden'); //Hide buttons before client loads
+
+</script>
+
+
+<script src="https://apis.google.com/js/client.js?onload=googleApiClientReady">
+</script>
+
+<script>
+
+    // When client loads, trigger action:
+
+    function googleApiClientReady() {
+        $('.YTSpagers').css('visibility', 'visible');//Buttons display when client ready
+    });
+    }
+
+</script>
 </html>
+
