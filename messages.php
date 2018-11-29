981cec0604f6b21b7f72cbb29bf36524f98aa421
William Ervin <william.ervin.14@cnu.edu>
2018-11-29 16:30:55 -0500

    youtube search

--- messages.php
+++ messages.php
@@ -136,7 +136,7 @@
         <div class="video-search-results">
             <?php foreach ($searchResponse['items'] as $searchResult) : ?>
                 <ul>
-                    <li><a href="youtube.php?uid=<?php echo $current_tab_user->ID; ?>/vid=<?php echo $searchResult['id']['videoId']; ?>"><?php echo $searchResult['snippet']['title']; ?></a></li>
+                    <li><a href="youtube.php?vid=<?php echo $searchResult['id']['videoId']; ?>"><?php echo $searchResult['snippet']['title']; ?></a></li>
                 </ul>
             <?php endforeach; ?>
         </div>
