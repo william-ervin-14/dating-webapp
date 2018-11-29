c4e9c9bc312f7bac4510960b8efc7844985d72c4
William Ervin <william.ervin.14@cnu.edu>
2018-11-29 15:41:03 -0500

    youtube search

--- youtube.php
+++ youtube.php
@@ -76,34 +76,5 @@
                 <?php endforeach; ?>
             </div>
         </form>
-
-        <script type="text/javascript">
-
-            function onPlayerReady(event) {
-                document.getElementById('existing-iframe').style.borderColor = '#FF6D00';
-            }
-            function changeBorderColor(playerStatus) {
-                var color;
-                if (playerStatus == -1) {
-                    color = "#37474F"; // unstarted = gray
-                } else if (playerStatus == 0) {
-                    color = "#FFFF00"; // ended = yellow
-                } else if (playerStatus == 1) {
-                    color = "#33691E"; // playing = green
-                } else if (playerStatus == 2) {
-                    color = "#DD2C00"; // paused = red
-                } else if (playerStatus == 3) {
-                    color = "#AA00FF"; // buffering = purple
-                } else if (playerStatus == 5) {
-                    color = "#FF6DOO"; // video cued = orange
-                }
-                if (color) {
-                    document.getElementById('existing-iframe-example').style.borderColor = color;
-                }
-            }
-            function onPlayerStateChange(event) {
-                changeBorderColor(event.data);
-            }
-        </script>
     </body>
 </html>
