a0e3155684b80599f60d03b75fdf39702c157e7e
William Ervin <william.ervin.14@cnu.edu>
2018-11-28 20:53:16 -0500

    youtube search

--- youtube.php
+++ youtube.php
@@ -16,6 +16,37 @@
     $user = $query->load_user_objects_by_email($email);
     $logged_user_id = ($user->ID);
 
+    if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+        throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
+    }
+
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    if (isset($_GET['q'])) {
+
+        $DEVELOPER_KEY = 'AIzaSyCDQM84XUFkyA6__WNdffCvmMzYoiaA6og';
+        $client = new Google_Client();
+        $client->setDeveloperKey($DEVELOPER_KEY);
+
+        $youtube = new Google_Service_YouTube($client);
+
+        $searchResponse = $youtube->search->listSearch('id,snippet', array(
+            'type' => 'video',
+            'q' => $_GET['q'],
+            'maxResults' => 25,
+        ));
+
+        $videos = '';
+        $thumbnails = array();
+
+        foreach ($searchResponse['items'] as $searchResult) {
+            $videos .= sprintf('<li>%s (%s)</li>',
+                $searchResult['snippet']['title'], $searchResult['id']['videoId']);
+        }
+        foreach ($searchResponse['items'] as $searchResult) {
+            $thumbnails = $searchResult['snippet']['thumbnails']['default'];
+        }
+    }
 ?>
 <html>
 <body>
@@ -25,14 +56,14 @@
         frameborder="0"
         style="border: solid 4px #37474F"
 ></iframe>
-<form action="http://www.youtube.com/results" method="get" target="_blank" >
-    <input name="search_query" type="text" maxlength="128" />
-    <select name="search_type">
-        <option value="">Videos</option>
-        <option value="search_users">Channels</option>
-    </select>
-    <input type="submit" value="Search" />
+<form method="GET">
+    <div>
+        Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+    </div>
+    <input type="submit" value="Search">
 </form>
+<h3>Videos</h3>
+<ul><?php echo $videos; ?></ul>
 
 
 <script type="text/javascript">
