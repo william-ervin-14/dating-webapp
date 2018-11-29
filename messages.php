57c208a598c4e11ef4c5ca6259f569f30d5cfbe6
William Ervin <william.ervin.14@cnu.edu>
2018-11-29 16:29:10 -0500

    youtube search

--- messages.php
+++ messages.php
@@ -46,6 +46,11 @@
         }
 
     }
+    if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+        throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
+    }
+
+    require_once __DIR__ . '/vendor/autoload.php';
     if (isset($_GET['q'])) {
 
         $DEVELOPER_KEY = 'AIzaSyCDQM84XUFkyA6__WNdffCvmMzYoiaA6og';
