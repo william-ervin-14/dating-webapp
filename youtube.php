002d8631c75b76e2b7cfd8aa8aa10b1721730863
William Ervin <william.ervin.14@cnu.edu>
2018-11-30 15:50:34 -0500

    youtube search

--- youtube.php
+++ youtube.php
@@ -109,36 +109,38 @@
             <button type="submit" name="exit_chat">Exit Chat</button>
         </form>
         <h3><?php echo $chat_id; ?></h3>
-        <div class ="chat-container">
-            <?php $messages_temp = $query->do_messages($message_received_objects, $message_sent_objects, $current_tab_user); ?>
-            <div class="top-name"><h4><?php echo "{$current_tab_user->firstname} {$current_tab_user->lastname}" ; ?></h4></div>
-            <?php foreach($messages_temp as $message_temp): ?>
-                <?php if(in_array($message_temp,$message_received_objects)) :?>
-                    <div class="message-box-received">
-                        <p><?php echo $message_temp->message_content; ?></p>
-                    </div>
+        <form method="post">
+            <div class ="chat-container">
+                <?php $messages_temp = $query->do_messages($message_received_objects, $message_sent_objects, $current_tab_user); ?>
+                <div class="top-name"><h4><?php echo "{$current_tab_user->firstname} {$current_tab_user->lastname}" ; ?></h4></div>
+                <?php foreach($messages_temp as $message_temp): ?>
+                    <?php if(in_array($message_temp,$message_received_objects)) :?>
+                        <div class="message-box-received">
+                            <p><?php echo $message_temp->message_content; ?></p>
+                        </div>
 
-                    <div class="message-time-received">
-                        <p><?php echo $message_temp->message_time; ?></p>
-                    </div>
-                <?php else : ?>
-                    <div class="message-box-sent">
-                        <p><?php echo $message_temp->message_content; ?></p>
-                    </div>
+                        <div class="message-time-received">
+                            <p><?php echo $message_temp->message_time; ?></p>
+                        </div>
+                    <?php else : ?>
+                        <div class="message-box-sent">
+                            <p><?php echo $message_temp->message_content; ?></p>
+                        </div>
 
-                    <div class="message-time-sent">
-                        <p><?php echo $message_temp->message_time; ?></p>
-                    </div>
-                <?php endif ?>
-            <?php endforeach; ?>
-            <div class="send-message-form">
-                <input name="message_time" type="hidden" value="<?php echo time(); ?>" />
-                <input name="message_sender_id" type="hidden" value="<?php echo $logged_user_id; ?>" />
-                <input name="message_recipient_id" type="hidden" value="<?php echo $current_tab_user->ID; ?>" />
-                <input class="message_input" name="message_content" type="text" placeholder="Your message">
-                <button class="submit_button" type="submit" value="Submit">Send</button>
+                        <div class="message-time-sent">
+                            <p><?php echo $message_temp->message_time; ?></p>
+                        </div>
+                    <?php endif ?>
+                <?php endforeach; ?>
+                <div class="send-message-form">
+                    <input name="message_time" type="hidden" value="<?php echo time(); ?>" />
+                    <input name="message_sender_id" type="hidden" value="<?php echo $logged_user_id; ?>" />
+                    <input name="message_recipient_id" type="hidden" value="<?php echo $current_tab_user->ID; ?>" />
+                    <input class="message_input" name="message_content" type="text" placeholder="Your message">
+                    <button class="submit_button" type="submit" value="Submit">Send</button>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
     </body>
 </html>
