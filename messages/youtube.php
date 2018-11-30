19572bce8f9c15f029c6f28bc5715fd5b0a59b39
William Ervin <william.ervin.14@cnu.edu>
2018-11-30 15:36:59 -0500

    youtube search

--- messages/youtube.php
+++ messages/youtube.php
@@ -1,15 +1,10 @@
 <?php
 
-    require_once('load.php');
+    require_once('../load.php');
 
     if (!isset($_SESSION['email'])) {
         $_SESSION['msg'] = "You must log in first";
-        header('location: login.php');
-    }
-    if (isset($_GET['logout'])) {
-        unset($_SESSION['email']);
-        session_destroy();
-        header("location: login.php");
+        header('location: ../login.php');
     }
     $email = $_SESSION['email'];
     $user = $query->load_user_objects_by_email($email);
