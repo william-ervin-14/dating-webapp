a037333f1eaf939826c820d82d91340e45e01be3
William Ervin <william.ervin.14@cnu.edu>
2018-12-01 18:34:23 -0500

    invitation/notifications

--- invitations.php
+++ invitations.php
@@ -14,7 +14,7 @@ if (isset($_GET['logout'])) {
 $email = $_SESSION['email'];
 $user = $query->load_user_objects_by_email($email);
 $logged_user_id = ($user->ID);
-$invitations =$query->get_invitations($logged_user_id)
+$invitations = $query->get_invitations($logged_user_id)
 ?>
 <html>
     <head>
@@ -22,18 +22,16 @@ $invitations =$query->get_invitations($logged_user_id)
     </head>
 
     <h1>Invitations</h1>
-    <div class="content">
-        <?php if("No invitations found" == $invitations): ?>
-            <h3>No invitations found</h3>
-        <? else : ?>
-            <?php foreach ( $invitations as $invitation ) : ?>
-                <?php $friend = $this->load_user_object($invitation->friend_id)?>
-                <div class="invitation-item">
-                    <h3><?php echo "{$friend->firstname} {$friend->lastname}"; ?></h3>
-                    <input type="submit" name="accept_invitation" value="Accept"/>
-                    <input type="submit" name="delete_invitation" value="Delete"/>
-                </div>
-            <?php endforeach;?>
-        <?php endif; ?>
-    </div>
+    <?php if("No invitations found" == $invitations): ?>
+        <h3>No invitations found</h3>
+    <? else : ?>
+        <?php foreach ( $invitations as $invitation ) : ?>
+            <?php $friend = $this->load_user_object($invitation->friend_id)?>
+            <div class="invitation-item">
+                <h3><?php echo "{$friend->firstname} {$friend->lastname}"; ?></h3>
+                <input type="submit" name="accept_invitation" value="Accept"/>
+                <input type="submit" name="delete_invitation" value="Delete"/>
+            </div>
+        <?php endforeach;?>
+    <?php endif; ?>
 </html>
