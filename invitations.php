db1f3e13126fb1ceef466a50acc866144623750e
William Ervin <william.ervin.14@cnu.edu>
2018-12-01 19:09:04 -0500

    invitation/notifications

--- invitations.php
+++ invitations.php
@@ -1,20 +1,26 @@
 <?php
-require_once('load.php');
-include('includes/header.php');
+    require_once('load.php');
+    include('includes/header.php');
 
-if (!isset($_SESSION['email'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('location: login.php');
-}
-if (isset($_GET['logout'])) {
-    unset($_SESSION['email']);
-    session_destroy();
-    header("location: login.php");
-}
-$email = $_SESSION['email'];
-$user = $query->load_user_objects_by_email($email);
-$logged_user_id = ($user->ID);
-$invitations = $query->get_invitations($logged_user_id)
+    if (!isset($_SESSION['email'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+    if (isset($_GET['logout'])) {
+        unset($_SESSION['email']);
+        session_destroy();
+        header("location: login.php");
+    }
+    $email = $_SESSION['email'];
+    $user = $query->load_user_objects_by_email($email);
+    $logged_user_id = ($user->ID);
+    $invitations = $query->get_invitations($logged_user_id);
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['accept_invitation'])){
+
+        }
+    }
 ?>
 <html>
     <head>
@@ -22,18 +28,20 @@ $invitations = $query->get_invitations($logged_user_id)
     </head>
 
     <h1>Invitations</h1>
-    <div class="content">
-    <?php if("No invitations found" == $invitations): ?>
-        <h3>No invitations found</h3>
-    <?php else : ?>
-        <?php foreach ( $invitations as $invitation ) : ?>
-            <?php $friend = $query->load_user_object($invitation->friend_id)?>
-            <div class="invitation-item">
-                <h3><?php echo "{$friend->firstname} {$friend->lastname}"; ?></h3>
-                <input type="submit" name="accept_invitation" value="Accept"/>
-                <input type="submit" name="delete_invitation" value="Delete"/>
-            </div>
-        <?php endforeach;?>
-    <?php endif; ?>
-    </div>
+    <form method="post">
+        <div class="content">
+        <?php if("No invitations found" == $invitations): ?>
+            <h3>No invitations found</h3>
+        <?php else : ?>
+            <?php foreach ( $invitations as $invitation ) : ?>
+                <?php $friend = $query->load_user_object($invitation->friend_id)?>
+                <div class="invitation-item">
+                    <h3><?php echo "{$friend->firstname} {$friend->lastname}"; ?></h3>
+                    <input type="submit" name="accept_invitation" value="Accept"/>
+                    <input type="submit" name="delete_invitation" value="Delete"/>
+                </div>
+            <?php endforeach;?>
+        <?php endif; ?>
+        </div>
+    </form>
 </html>
--- notifications.php
+++ notifications.php
@@ -1,26 +1,39 @@
 <?php
-require_once('load.php');
-include('includes/header.php');
+    require_once('load.php');
+    include('includes/header.php');
 
-if (!isset($_SESSION['email'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('location: login.php');
-}
-if (isset($_GET['logout'])) {
-    unset($_SESSION['email']);
-    session_destroy();
-    header("location: login.php");
-}
-$email = $_SESSION['email'];
-$user = $query->load_user_objects_by_email($email);
-$logged_user_id = ($user->ID);
+    if (!isset($_SESSION['email'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+    if (isset($_GET['logout'])) {
+        unset($_SESSION['email']);
+        session_destroy();
+        header("location: login.php");
+    }
+    $email = $_SESSION['email'];
+    $user = $query->load_user_objects_by_email($email);
+    $logged_user_id = ($user->ID);
+    $notifications = $query->get_notifications($logged_user_id)
 ?>
 <html>
-    <head>
-        <title>Notifications</title>
-    </head>
-    <h1>Notifications</h1>
+<head>
+    <title>Notifications</title>
+</head>
+
+<h1>Notifications</h1>
+<form method="post">
     <div class="content">
-        <?php $query->do_notifications($logged_user_id); ?>
+        <?php if("No notifications found" == $invitations): ?>
+            <h3>No notifications found</h3>
+        <?php else : ?>
+            <?php foreach ( $notifications as $notification ) : ?>
+                <?php $friend = $query->load_user_object($notification->friend_id)?>
+                <div class="notification-item">
+                    <h3><?php echo "{$friend->firstname} {$friend->lastname}"; ?></h3>
+                </div>
+            <?php endforeach;?>
+        <?php endif; ?>
     </div>
+</form>
 </html>
