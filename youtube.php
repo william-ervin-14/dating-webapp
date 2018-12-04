ba5c7593d62e27d1294b429be8d7ab973488e63b
William Ervin <william.ervin.14@cnu.edu>
2018-12-04 00:16:17 -0500

    invitation/notifications

--- youtube.php
+++ youtube.php
@@ -101,7 +101,7 @@
             </div>
         </form>
         <iframe id="existing-iframe"
-                width="50%" height="99%" align="left"
+                width="50%" height="97%" align="left"
                 src="https://www.youtube.com/embed/<?php echo $current_video_id; ?>?enablejsapi=1"
                 frameborder="0"
                 style="border: solid 4px #37474F"
